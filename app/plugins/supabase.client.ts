b372d68fd7e4e34131e04b461ad9dc3a4c378b66
IlhamKurniawanBlora <ilhamkurniawanjateng@gmail.com>
2025-06-09 04:31:07 +0700

    fix supabase config

--- app/plugins/supabase.client.ts
+++ app/plugins/supabase.client.ts
@@ -1,15 +1,15 @@
-// // app/plugins/supabase.client.ts
+// app/plugins/supabase.client.ts
 
-// import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js'
 
-// export default defineNuxtPlugin((nuxtApp) => {
-//   const config = useRuntimeConfig()
+export default defineNuxtPlugin((nuxtApp) => {
+  const config = useRuntimeConfig()
 
-//   const supabaseUrl = config.public.supabaseUrl
-//   const supabaseKey = config.public.supabaseKey
+  const supabaseUrl = config.public.supabaseUrl
+  const supabaseKey = config.public.supabaseKey
 
-//   const supabase = createClient(supabaseUrl, supabaseKey)
+  const supabase = createClient(supabaseUrl, supabaseKey)
 
-//   // Biar bisa diakses di seluruh app pakai `useSupabaseClient`
-//   nuxtApp.provide('supabase', supabase)
-// })
+  // Biar bisa diakses di seluruh app pakai `useSupabaseClient`
+  nuxtApp.provide('supabase', supabase)
+})
